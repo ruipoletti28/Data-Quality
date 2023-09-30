f2816b5124632b034d2d675f464cb4939c079d78
Rui <ruijr22jr@gmail.com>
2023-09-30 15:32:43 -0300

    30_09 - Concatenação e Parametros forms via post

---
+++ fileupload/static/myscript.js
@@ -0,0 +1,227 @@
+//import functionB from './ClasseB.js';
+//import functionB from './ClasseB.js';
+//import pegarValorCripto from './criptoPlanilha.js';
+//import pegarValorSaida from './saidaPlanilha.js';
+const preview = document.querySelector('.visualizacao'); //botão de upload
+const input = document.querySelector('input');//input recebe o file, planilha no caso
+const CriptoSim = document.querySelectorAll('input[type="checkbox"]'); //checkbox para pop-up do chekbox
+const selecaoCripto = document.querySelectorAll('input[name="emailNao"]');//busca o select de saida no html
+const popup = document.querySelector('.popup'); //pop-up de arquivo não feito upload
+var selecaoFormato = document.getElementById('formatoSaida');//busca o select de saida no html
+
+var countArchives = 0;
+input.style.opacity = 0;
+
+input.addEventListener('change', updateImageDisplay);
+
+function updateImageDisplay() {
+  
+  while(preview.firstChild) {
+    preview.removeChild(preview.firstChild);
+  }
+
+  const curFiles = input.files;
+  if(curFiles.length === 0) {
+    //const para = document.createElement('p');
+    alert("Nenhum arquivo selecionado para upload");
+  } else {
+    const list = document.createElement('ol');
+    preview.appendChild(list);
+  }
+  for(const file of curFiles) {
+      
+    const listItem = document.createElement('li');
+    const para = document.createElement('p');
+
+    if(validFileType(file)) {     
+
+      switch (getFileExtension(file.name)){
+
+        case '.csv':
+
+        //functionB(nomePlanilha);
+
+          var leitor = new FileReader();
+          
+          leitor.onload = function(e) {
+            var linhas = e.target.result.split("\n");
+            var colunas = linhas[0].split(",");
+            var celula = linhas[1].split(","); // Lê a segunda linha como exemplo
+            var nomeColuna = colunas[0]; // Nome da coluna 1
+            var numeroLinha = 2; // Número da linha 1
+            var conteudoCelula = celula[0]; // Conteúdo da célula 1,1 
+            document.getElementById( 
+              "demo"+countArchives).innerHTML = (`Arquivo: <strong> ${file.name} n° </strong>`
+                + countArchives 
+                  + "<br>" 
+                    + "Nome da coluna: " 
+                      + nomeColuna 
+                        + "<br>" 
+                          + "Linha N°: "
+                            + numeroLinha 
+                              + "<br>" 
+                                + "Conteudo da celula: " 
+                                  + conteudoCelula); 
+            //saveFile(curFiles);
+            return {nomeColuna, numeroLinha, conteudoCelula};
+          }
+
+          leitor.readAsText(file, function(nomeColuna, numeroLinha, conteudoCelula){
+            console.log(nomeColuna, numeroLinha, conteudoCelula);
+          });
+        break;
+        
+        case '.xlsx':
+
+        //functionB(nomePlanilha);
+          var reader = new FileReader();
+          
+          reader.onload = function(e) {
+            var data = new Uint8Array(e.target.result);
+            var workbook = XLSX.read(data, { type: 'array' });
+            
+            // seleciona a planilha que deseja ler
+            var sheetName = workbook.SheetNames[0];
+            var worksheet = workbook.Sheets[sheetName];
+
+            // seleciona a coluna e a linha que deseja mostrar
+            var cellAddress = 'A2';
+            var colAddress = 'A1';
+            var cell = worksheet[cellAddress];
+            var colName = worksheet[colAddress];
+            //var colIndex = cell ? cell.c : undefined; // obtém o índice da coluna
+            //var colName = colIndex !== undefined ? XLSX.utils.decode_col(colIndex) : undefined; // decodifica o índice em nome de coluna
+            var value = cell ? cell.v : undefined;
+            var valueCol = colName ? colName.v : undefined;
+
+            console.log(`O valor da célula ${cellAddress} é ${value} na coluna ${valueCol}`);
+
+            // exibe o valor na tela
+            document.getElementById("demo" +countArchives).innerHTML = (`Arquivo: <strong> ${file.name} n° </strong>`
+            + countArchives
+                + "<br>" 
+                  + ' Nome da coluna: '
+                    + valueCol 
+                      + "<br>" 
+                        +'Linha N°: '
+                          + cellAddress 
+                            + "<br>"
+                              + 'Conteudo da celula: ' 
+                                + value);
+          //saveFile(curFiles);
+          };
+          reader.readAsArrayBuffer(this.files[0]);
+
+        break;
+
+        case false:
+          para.textContent = `Arquivo ${file.name}: Arquivo não compatível. Selecione um arquivo .xls, xlsx, .csv.`;
+        break;
+      
+      }
+
+      para.textContent = `Arquivo ${file.name}, tipo ${getFileExtension(file.name)}, tamanho do arquivo ${returnFileSize(file.size)}.`, countArchives++;
+      //alert("File: " + file.listItem);
+      //console.log(`Arquivo: ${file.name} n°`+ countArchives);
+
+      listItem.appendChild(para);
+      returnFileSize(file);
+      //returnQntdArchives(file); 
+    } else {
+      para.textContent = `Arquivo ${file.name}: Arquivo não compatível. Selecione um arquivo .xls, xlsx, .csv.`;
+      listItem.appendChild(para);
+    }
+
+    //listItem.appendChild(listItem);
+  }
+}
+  function getFileExtension(filename) {
+    return filename.slice((filename.lastIndexOf(".") - 1 >>> 0) + 1);
+  }
+
+  function saveFile(curFiles) {
+    
+    const folderName = "Data Quality"; // nome da pasta
+    const folderPath = "C:/Temp"; // caminho da pasta
+    window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
+    window.requestFileSystem(window.PERSISTENT, 5*1024*1024, function(fs) {
+      
+      fs.root.getDirectory(folderName, {create: true}, function(dirEntry) {
+       
+        for(const file of curFiles) {
+          
+          const fileName = file.name;
+          
+          dirEntry.getFile(fileName, {create: true}, function(fileEntry) {
+            fileEntry.createWriter(function(fileWriter) {
+              fileWriter.write(file);
+              console.log(`Arquivo salvo em: ${folderPath}, ${fileName}`);
+            });
+          });
+        }
+      });
+    });
+  }
+  
+  /*selecaoFormato.addEventListener('change', function(valueSaida){
+    var valueSaida = selecaoFormato.value;
+    pegarValorSaida(valueSaida);
+  })
+
+  selecaoCripto.forEach((name) => {
+    name.addEventListener('click', () => {
+      var valueSaidaCripto = selecaoCripto.name;
+      if(name.checked){
+        pegarValorCripto(valueSaidaCripto);
+      } else {
+        console.log("Marque uma opção!");
+      }
+    })
+  });*/
+        
+  const fileTypes = [
+      "application/excel", //xls
+      "application/vnd.ms-excel",//xls
+      "application/msexcel",//xls
+      "application/vnd.ms-excel", //xls
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", //xslx
+      "text/csv", //csv
+      "text/x-comma-separated-values",//csv
+      "text/comma-separated-values", //csv
+      "application/octet-stream", //csv 
+      "text/csv", //csv
+  ]
+
+  function validFileType(file) {
+    //alert("File: " + file.type);
+    //alert("Resultado: " + fileTypes.includes(file.type));
+    return fileTypes.includes(file.type);
+  }
+
+  function returnFileSize(number) {
+    if(number < 1024) {
+      return number + 'bytes';
+    } else if(number > 1024 && number < 1048576) {
+      return (number/1024).toFixed(1) + 'KB';
+    } else if(number > 1048576) {
+      return (number/1048576).toFixed(1) + 'MB';
+    }
+  }
+
+  CriptoSim.forEach((radio) => {
+    radio.addEventListener('click', () => {
+      if(radio.checked){
+        alert("O arquivo será criptografado! \n A chave será gerada ao fim do processo.");
+      } else {
+        popup.style.display = 'none';
+      }
+    })
+  });
+
+  window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
+
+  window.requestFileSystem(window.TEMPORARY, 5 * 1024 * 1024, function(fs) {
+    fs.root.getDirectory('"C:/Users/Ubuntu/Downloads/Data Quality"', {create: true}, function(dirEntry) {
+      // aqui você pode copiar o arquivo para a pasta de destino
+    });
+  });
--- fileupload/upload.py
+++ fileupload/upload.py
@@ -1,54 +1,102 @@
-from flask import Flask, request, render_template, send_file
+from flask import Flask, request, render_template, send_file, Response
 import os
-import pyautogui as pag
 import pandas as pd
 
-app = Flask(__name__)
+UPLOAD_FOLDER = 'fileupload/uploads'
+ALLOWED_EXTENSIONS = {'csv', 'xlsx', 'xls'}
+MAX_FILES = 4
+
+app = Flask(__name__, static_folder='static')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 @app.route('/')
 def index():
-    return render_template('upload.html')  # Página de upload
+    return render_template('home.html')  # Página de upload
+
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    uploaded_file = request.files['file']
-    if uploaded_file.filename != '':
-        file_extension = uploaded_file.filename.split('.')[-1]
-        if file_extension in ['csv']:
-            df = pd.read_csv(uploaded_file)
-            # Processar e editar o DataFrame df conforme necessário
-            
-            df['MES_ANIVERSARIO'] = 1
-            
-            # Salvar DataFrame editado em um novo arquivo CSV
-            df.to_csv('edited_data.csv', index=False)
-            
-            return render_template('upload.html')
+    """
+    This function handles file uploads and processing. It accepts POST requests with files attached, and processes them
+    based on their file extension. CSV files are read into a Pandas DataFrame, have a column added, and are saved as a new
+    CSV file. XLSX files are also read into a Pandas DataFrame, have a column added, and are saved as a new XLSX file.
+    The resulting DataFrames are combined into a single DataFrame, which is saved as a CSV file. Finally, the user is
+    redirected to a download page.
+
+    Returns:
+        If the number of uploaded files is within the allowed limit, the function returns a rendered download.html
+        template. Otherwise, it returns a string indicating that the number of files exceeds the limit.
+    """
+    uploaded_files = request.files.getlist('arquivo')
+    processed_files = []
+    formato_saida = request.form.get('formatoSaida')
+
+    if len(uploaded_files) <= MAX_FILES:
+        combined_df = pd.DataFrame()  # Crie um DataFrame vazio para combinar as planilhas
+        FileXlsx = None
+        FileCsv = None
+        for uploaded_file in uploaded_files:
+            if uploaded_file.filename != '':
+                file_extension = uploaded_file.filename.split('.')[-1]
+                if file_extension in ALLOWED_EXTENSIONS:
+                    if file_extension in ['csv']:
+                        FileCsv = pd.read_csv(uploaded_file)
+                        FileCsv['MES_ANIVERSARIO'] = 2
+                        #edited_file_name = uploaded_file.filename.replace('.csv', '_Editado.csv')
+                        #edited_file_path = os.path.join(app.config['UPLOAD_FOLDER'], edited_file_name)
+                        #FileCsv.to_csv(edited_file_path, index=False)
+                        #processed_files.append(edited_file_path)
+                        
+                        #print(FileCsv.head())  # Mostra as primeiras linhas do DataFrame
+                        #print(FileCsv.tail())  # Mostra as últimas linhas do DataFrame
+                        combined_df = pd.concat([combined_df, FileCsv], ignore_index=True)
+
+                    elif file_extension in ['xlsx']:
+
+                        FileXlsx = pd.read_excel(uploaded_file)
+                        FileXlsx['Mes de Aniversario'] = 3
+                        #edited_file_name = uploaded_file.filename.replace('xlsx', '_Editado.xlsx')
+                        #edited_file_path = os.path.join(app.config['UPLOAD_FOLDER'], edited_file_name)
+                        #FileXlsx.to_excel(edited_file_path, index=False)
+                        #processed_files.append(edited_file_path)
+
+                        combined_df = pd.concat([combined_df, FileXlsx], ignore_index=True)
+
+        if formato_saida == "xlsx":
+            # Salvar a planilha combinada em formato XLSX
+            combined_file_path = os.path.join(app.config['UPLOAD_FOLDER'], 'combined.xlsx')
+            combined_df.to_excel(combined_file_path, index=False)
+            processed_files.append(combined_file_path)
+        elif formato_saida == "csv":
+            # Salvar a planilha combinada em formato XLSX
+            combined_file_path = os.path.join(app.config['UPLOAD_FOLDER'], 'combined.csv')
+            combined_df.to_csv(combined_file_path, index=False)
+            processed_files.append(combined_file_path)
+
+        return render_template('download.html')
+
+    return 'Número de arquivos excede o limite permitido.'
+
+
+"""@app.route('/download')
+def download(processed_files):
+    if len(processed_files) > 0:
+        zip_filename = 'Alteração.zip'
+        with zipfile.ZipFile(zip_filename, 'w') as zipf:
+            for file in processed_files:
+                zipf.write(file, os.path.basename(file))
+
+        def generate():
+            with open(zip_filename, 'rb') as f:
+                yield from f.read()
+            os.remove(zip_filename)
         
-        if file_extension in ['xlsx']:
-            df = pd.read_excel(uploaded_file)
-            # Processar e editar o DataFrame df conforme necessário
-
-            df['Mes de Aniversario'] = 2
-
-            df.to_excel('data_edited_xslx.xlsx', index=False)
-
-            #pag.alert(text="Hello World", title="The Hello World Box")
-
-            return render_template('upload.html')
+        response = Response(generate(), content_type='application/zip')
+        response.headers['Content-Disposition'] = f'attachment; filename={zip_filename}'
+        return response
     
-    return 'Formato de arquivo não suportado.'
-
-
-@app.route('/download')
-def download():
-    edited_file_path = 'data_edited_xslx.xlsx'  # Altere o nome do arquivo conforme necessário
-    try:
-        return send_file(edited_file_path, as_attachment=True)
-    finally:
-        os.remove(edited_file_path)  # Exclui o arquivo após o download
- 
+    return 'Nenhum arquivo processado para download.'"""
 
 if __name__ == '__main__':
     app.run(debug=True)
-
